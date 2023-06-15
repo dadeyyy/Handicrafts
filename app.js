72c57319c69dd3e1a499a422899c06bdb157dbaa
dadeyyy <93317287+dadeyyy@users.noreply.github.com>
2023-06-15 12:07:54 +0800

    final commit

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ if (process.env.NODE_env !== 'production') {
   require('dotenv').config();
 }
 
-
 const express = require('express');
 const app = express();
 const PORT = 3000;
@@ -19,11 +18,11 @@ const User = require('./models/user');
 const handicraftRoutes = require('./routes/handicrafts');
 const reviewRoutes = require('./routes/reviews');
 const userRoutes = require('./routes/users');
-const adminRoutes = require('./routes/admin')
+const adminRoutes = require('./routes/admin');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const Handicraft = require('./models/handicraft')
+const Handicraft = require('./models/handicraft');
 const mongoSanitize = require('express-mongo-sanitize');
-
+const helmet = require('helmet');
 mongoose.set('strictQuery', true);
 async function main() {
   await mongoose.connect('mongodb://localhost:27017/geolocation');
@@ -31,8 +30,6 @@ async function main() {
 }
 
 main().catch((err) => console.log(err));
-
-
 
 const sessionConfig = {
   name: 'session',
@@ -50,20 +47,61 @@ app.use(session(sessionConfig));
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(mongoSanitize())
 
+app.use(mongoSanitize());
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net/",
+    "https://res.cloudinary.com/dv5vm4sqh/"
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net/",
+    "https://res.cloudinary.com/dv5vm4sqh/"
+];
+const connectSrcUrls = [
+    "https://*.tiles.mapbox.com",
+    "https://api.mapbox.com",
+    "https://events.mapbox.com",
+    "https://res.cloudinary.com/dv5vm4sqh/"
+];
+const fontSrcUrls = [ "https://res.cloudinary.com/dv5vm4sqh/" ];
+ 
+app.use(
+    helmet.contentSecurityPolicy({
+        directives : {
+            defaultSrc : [],
+            connectSrc : [ "'self'", ...connectSrcUrls ],
+            scriptSrc  : [ "'unsafe-inline'", "'self'", ...scriptSrcUrls ],
+            styleSrc   : [ "'self'", "'unsafe-inline'", ...styleSrcUrls ],
+            workerSrc  : [ "'self'", "blob:" ],
+            objectSrc  : [],
+            imgSrc     : [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/dmpip4nzo/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
+                "https://images.unsplash.com/"
+            ],
+            fontSrc    : [ "'self'", ...fontSrcUrls ],
+            mediaSrc   : [ "https://res.cloudinary.com/dv5vm4sqh/" ],
+            childSrc   : [ "blob:" ]
+        }
+    })
+);
 
 passport.use(new LocalStrategy(User.authenticate()));
-// passport.use(new GoogleStrategy({
-//   clientID: process.env.CLIENT_ID,
-//   clientSecret:process.env.CLIENT_SECRET,
-//   callbackURL:'http://localhost:3000/handicrafts',
-//   scope: ['profile', 'email']
-// },
-// (accessToken, refreshToken, profile, done) =>{
 
-// }
-// ))
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -86,17 +124,6 @@ app.use('/handicrafts', handicraftRoutes);
 app.use('/handicrafts/:id/reviews', reviewRoutes);
 app.use('/', userRoutes);
 app.use('/', adminRoutes);
-
-// app.get('/display',async (req,res)=>{
-//   const handicraft = await Handicraft.find({});
-//   for(let i = 0 ; i< handicraft.length; i ++){
-//     handicraft[i].isValidated = false;
-//     await handicraft[i].save();
-//   }
-//   res.redirect('/')
-// })
-
-
 
 app.all('*', (req, res, next) => {
   next(new ExpressError('Page Not Found', 404));
